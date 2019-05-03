6d63beb84dd7a1241226bd1d4f86f52469de2fa2
Helquist <38138473+Helquist@users.noreply.github.com>
2019-05-03 15:55:14 +0200

    Strefy odpalające poziomy
    Wchodząc w odpowiednią strefę da się uruchomić minigrę

---
+++ GrandTheftMiasteczko/Assets/Scripts/GameStartTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameStartTrigger : MonoBehaviour
+{
+    public string miniGame;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Debug.Log("lol");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(miniGame);
+    }
+}
