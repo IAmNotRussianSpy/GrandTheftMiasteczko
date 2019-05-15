7acff2993a496003db2b37ba840f3b6d85fd4125
Helquist <jacek.piskorz@gmail.com>
2019-05-15 17:05:19 +0200

    Podstawy GhostRidera

---
+++ GrandTheftMiasteczko/Assets/Scripts/GhostRider/JumpController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    public Vector2 jumpHeight;
+    public Rigidbody2D player;
+    private int jumps = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (player.position.y < -3.3)
+            {
+                jumps = 0;
+            }
+            if (jumps<2)
+            {
+                player.AddForce(jumpHeight, ForceMode2D.Impulse);
+                jumps++;
+            }
+        }
+        
+    }
+}
