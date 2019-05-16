f5c86d9d897becd280a5d50ff31ad1e53a7f8561
Helquist <jacek.piskorz@gmail.com>
2019-05-16 12:12:40 +0200

    Jedziem! (z lampami)

---
+++ GrandTheftMiasteczko/Assets/Scripts/GhostRider/MoveObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObj : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.x < -25.5f)
+        {
+            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y, 0);
+            gameObject.transform.position = gameObject.transform.position + new Vector3(21.29f, 0, 0);
+        }
+        gameObject.transform.position = gameObject.transform.position - new Vector3(speed, 0, 0);
+    }
+}
