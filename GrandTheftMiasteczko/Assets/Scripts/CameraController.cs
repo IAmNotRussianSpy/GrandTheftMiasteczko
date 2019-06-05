fb5d998f97ac6201741d03ca34ecfbbec9dbdd12
Helquist <38138473+Helquist@users.noreply.github.com>
2019-06-05 23:12:04 +0200

    Poprawiony ruch kamery po miasteczku

--- GrandTheftMiasteczko/Assets/Scripts/CameraController.cs
+++ GrandTheftMiasteczko/Assets/Scripts/CameraController.cs
@@ -5,7 +5,8 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;       //Public variable to store a reference to the player game object
-
+    private float width;
+    private float height;
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
@@ -14,12 +15,41 @@ public class CameraController : MonoBehaviour
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
+
+        height = 2 * Camera.main.orthographicSize;
+        width = height * Camera.main.aspect;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        var tmp = player.transform.position + offset;
+        double x;
+        if (tmp.x > 13.33 - width / 2)
+        {
+            x = 13.33 - (width / 2);
+        } else if(tmp.x < -41.74 + width / 2)
+        {
+            x = -41.74 + (width / 2);
+        } else
+        {
+            x = tmp.x;
+        }
+        double y;
+        if (tmp.y > 17.12 - height / 2)
+        {
+            y = 17.12 - (height / 2);
+        }
+        else if (tmp.y < -29.78 + height / 2)
+        {
+            y = -29.78 + (height / 2);
+        }
+        else
+        {
+            y = tmp.y;
+        }
+        Vector3 vector = new Vector3((float)x, (float)y, -23);
+        transform.position = vector;
     }
 }
